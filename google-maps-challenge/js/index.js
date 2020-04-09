aa37b18c3bb889c722c07ee43c4d89733a2ab731
Adam Hartman <adamhartman5@gmail.com>
2020-04-09 00:50:34 -0400

    Move initMap function to index.js

---
+++ google-maps-challenge/js/index.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var sydney = {lat: -33.863276, lng: 151.107977};
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: sydney,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+    });
+}
+
