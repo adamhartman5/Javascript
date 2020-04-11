7e2e68a135fdac811c43e0047de33504e822f2f6
Adam Hartman <adamhartman5@gmail.com>
2020-04-10 22:25:02 -0400

    Move store list to javascript file

--- google-maps-challenge/js/index.js
+++ google-maps-challenge/js/index.js
@@ -12,7 +12,26 @@ function initMap() {
 }
 
 function displayStores() {
+    var storesHtml = '';
     for(var store of stores){
-        console.log(store)
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-info-container">
+                    <div class="store-address">
+                        <span>8480 Beverly Blvd</span>
+                        <span>Los Angeles, CA 90048</span>
+                    </div>
+                    <div class="store-phone-number">
+                        727-867-5309
+                    </div>
+                </div>
+                <div class="store-number-container">
+                    <div class="store-number">
+                        1
+                    </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml
     }
 }
